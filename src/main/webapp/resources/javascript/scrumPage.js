a1ea19ccca9cab8c6d764e2cfe3344051caa7c82
Hendrik <hendrik.kolver@gmail.com>
2013-06-13 13:55:38 +0200

    Bug fix attempt.  Tabs not working

--- src/main/webapp/resources/javascript/scrumPage.js
+++ src/main/webapp/resources/javascript/scrumPage.js
@@ -1,25 +1,29 @@
-    
-        $(document).ready(function()
-	{	
+ $(document).ready(function()
+	{
+               
+            
 		/*Called every time the text area is changed, TODO: Update with WebSockets*/
 		$('.ta').bind('input propertychange', function() {
 			//alert($(this).val());
-                        ws.send('text,'+ $(this).val() + "," +$(this).next().attr('id'));
+                        ws.send('text,'+ $(this).val() + "," +$(this).next().attr('id')+','+$(this).parent().parent().attr('id'));
+                        
 		});
-			   
+
                 
 		$('.snapHere').droppable({
 		   drop: function(ev, ui) 
 		   {
-			$(ui.draggable).offset({ top: ($(this).offset().top+5), left: ($(this).offset().left+5)});
+                       var relativeTop = $("#DragContainer").offset().top - $(this).offset().top;
+                       var relativeLeft = $("#DragContainer").offset().left - $(this).offset().left;
+			$(ui.draggable).offset({ top: ($(this).offset().top), left: ($(this).offset().left)});
                         var thisPos = $(this).position();
-                        var y = thisPos.left+5;
-                        var x = thisPos.top+5;
+                        var y = thisPos.left;
+                        var x = thisPos.top;
                            
                         ws.send('position,'+x + "," + y + "," +$(ui.draggable).attr('id'));
 		    }
 		});
-		
+
 		$(".edit").prev().children(":first").hide();
 		$(".edit").prev().children(":nth-child(2)").html($(".edit").prev().children(":first").val());
                 $(".edit").each(function(){
@@ -31,9 +35,9 @@
                 });
 		$(".edit").click(function()
 		{
-			if ($(this).text() === "Edit")
+			if ($(this).text() == "Edit")
 			{
-				$(this).text("Done Editing");
+				$(this).text("Done Editting");
 				$(this).prev().children(":first").show();
 				$(this).prev().children(":first").val($(this).prev().children(":nth-child(2)").text());
 				$(this).prev().children(":nth-child(2)").hide();
@@ -51,7 +55,10 @@
                     
                     
                 var current = null;   
+                
+                //recieve server return message
                 function showMessage(text) {
+                   // alert(text);
                     var chars = text.split(',');
                     var text = (chars[0] +',' + chars[1]);
                     
@@ -62,7 +69,7 @@
                         $(id).animate({
                             top: '' + chars[1] +'',
                             left: '' + chars[2]+''  
-                        }, 35, function() {
+                        }, 300, function() {
                             
                         }); 
                            
@@ -72,6 +79,7 @@
                         
                         var line = ""+chars[1]+"";
                         var id= "#"+chars[2];
+                        //alert(line)
                         //alert(id);
                         $(id).html(line);
                     }
@@ -89,26 +97,6 @@
                 $('.draggable').draggable( {
                     cursor: 'move',
                     containment: 'document',
-                    drag: function() {
-
-                        var $this = $(this);
-                        current = $this.attr('id')
-                        var thisPos = $this.position();
-                        var parentPos = $this.parent().position();
-                        
-                        var y = thisPos.left;// - parentPos.left;
-                        var x = thisPos.top;// - parentPos.top;
-                        
-
-                        if(dragger > 10)
-                        {
-                           // alert(x)
-                                ws.send('position,'+x + "," + y + "," +$this.attr('id'));
-                                dragger = 0;
-                        }
-
-                        dragger++;
-                        },
                     stop: function(){
                     current = null; 
                     }
