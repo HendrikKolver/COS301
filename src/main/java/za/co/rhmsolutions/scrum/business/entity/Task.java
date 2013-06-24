fb2bc74b84fd67d45aae7efc19bc92c25953af01
Richard O'Brien <sugarfree.producer@gmail.com>
2013-06-24 12:39:58 +0200

    Added Task Entity

--- src/main/java/za/co/rhmsolutions/scrum/business/entity/Task.java
+++ src/main/java/za/co/rhmsolutions/scrum/business/entity/Task.java
@@ -4,10 +4,32 @@
  */
 package za.co.rhmsolutions.scrum.business.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  *
  * @author Richard
+ * Boundaries are able to access entities directly
+ * Anything that is persistent is placed in entity package
+ * Entity: Persistent
  */
-public class Task {
+@Entity
+public class Task //persistent
+{
+    @Id
+    @GeneratedValue
+    private long id;
     
+    private String name;
+
+    /*needed for JPA*/
+    public Task()
+    {
+    }
+
+    public Task(String name) {
+        this.name = name;
+    }   
 }
