b487ac9bf12f8f4261b7281ffb0c4ff7a6d4fc13
Richard O'Brien <sugarfree.producer@gmail.com>
2013-06-24 11:59:05 +0200

    Added backend files

---
+++ src/main/java/za/co/rhmsolutions/scrum/business/boundary/TeskServices.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.co.rhmsolutions.scrum.business.boundary;
+
+/**
+ *
+ * @author Richard
+ */
+public class TeskServices {
+    
+}
---
+++ src/main/java/za/co/rhmsolutions/scrum/presentation/Index.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.co.rhmsolutions.scrum.presentation;
+
+/**
+ *
+ * @author Richard
+ */
+public class Index {
+    
+}
