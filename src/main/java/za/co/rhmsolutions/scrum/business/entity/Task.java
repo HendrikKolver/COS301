0501b2f8f6a294fe7ad671d3435d3c16f7200de2
Richard O'Brien <sugarfree.producer@gmail.com>
2013-06-28 10:19:39 +0200

    Database functions

--- src/main/java/za/co/rhmsolutions/scrum/business/entity/Task.java
+++ src/main/java/za/co/rhmsolutions/scrum/business/entity/Task.java
@@ -21,15 +21,51 @@ public class Task //persistent
     @Id
     @GeneratedValue
     private long id;
-    
-    private String name;
+
+    String name;
+    String topPos;
+    String leftPos;
+    String status;
+    String description;
+    String responsible;
+    String points;
+    String days;
+    String colour;
 
     /*needed for JPA*/
     public Task()
     {
+        name = "TaskName";
+        topPos = "topPos";
+        leftPos = "leftPos";
+        status = "status";
+        description = "description";
+        responsible = "responsible";
+        points = "points";
+        days = "days";
+        colour = "colour";
     }
 
     public Task(String name) {
         this.name = name;
-    }   
+    }
+    
+    public Task(String name, String topPos, String leftPos, 
+            String status, String description, String responsible, String points, String days, String colour) {
+        System.out.println("name: "+name);
+        this.name = name;
+        this.topPos = topPos;
+        this.leftPos = leftPos;
+        this.status = status;
+        this.description = description;
+        this.responsible = responsible;
+        this.points = points;
+        this.days = days;
+        this.colour = colour;
+    }
+    
+    public long getID()
+    {
+        return id;
+    }
 }
--- src/main/java/za/co/rhmsolutions/scrum/presentation/index.java
+++ src/main/java/za/co/rhmsolutions/scrum/presentation/index.java
@@ -19,6 +19,32 @@ public class index
     TaskService ts;
     String name;
     String text;
+    String topPos = "topPos";
+    String leftPos = "leftPos";
+    String status = "status";
+    String description = "description";
+    String responsible = "responsible";
+    String points = "points";
+    String days = "days";
+    String colour = "colour";
+    String updateID;
+    String deleteID;
+
+    public String getDeleteID() {
+        return deleteID;
+    }
+
+    public void setDeleteID(String deleteID) {
+        this.deleteID = deleteID;
+    }
+
+    public String getUpdateID() {
+        return updateID;
+    }
+
+    public void setUpdateID(String updateID) {
+        this.updateID = updateID;
+    }
 
     public String getName() {
         return name;
@@ -36,7 +62,7 @@ public class index
         //this.text = text;
     }
     
-    
+    //Creates new task for all new changes using values in w. Not for production purposes
     public void createTask()
     {
         System.out.println("Create Task Clicked!");
@@ -45,15 +71,82 @@ public class index
         {
             if(w.getTasks().get(x).getUpdate())
             {
-                name = w.getTasks().get(x).getID();
-                
+                name = w.getTasks().get(x).getName();
+                topPos = w.getTasks().get(x).getTopPos();
+                leftPos = w.getTasks().get(x).getLeftPos();
+                status = w.getTasks().get(x).getStatus();
+                description = w.getTasks().get(x).getDescription();
+                responsible = w.getTasks().get(x).getResponsible();
+                points = w.getTasks().get(x).getPoints();
+                days = w.getTasks().get(x).getDays();
+                colour = w.getTasks().get(x).getColour();
 
-                //System.out.println(name + "; "+ text);
+                ts.create(name, topPos, leftPos, status, description, responsible, points, days, colour);
                 
-                ts.create(name);
                 w.getTasks().get(x).dbUpdate();
             }
         }
+    }
+    
+    //Update task using value of updateID
+    public void update()
+    {
+        WebSockets w = Reference.w;
+        for(int x=0; x< w.getTasks().size();x++)
+        {
+            if(w.getTasks().get(x).getUpdate() && w.getTasks().get(x).getID().equals(updateID))
+            {
+                name = w.getTasks().get(x).getName();
+                topPos = w.getTasks().get(x).getTopPos();
+                leftPos = w.getTasks().get(x).getLeftPos();
+                status = w.getTasks().get(x).getStatus();
+                description = w.getTasks().get(x).getDescription();
+                responsible = w.getTasks().get(x).getResponsible();
+                points = w.getTasks().get(x).getPoints();
+                days = w.getTasks().get(x).getDays();
+                colour = w.getTasks().get(x).getColour();
+
+                long id;
+                id = Long.valueOf(updateID).longValue();
+                
+                ts.update(id, name, topPos, leftPos, status, description, responsible, points, days, colour);
+                System.out.println("Updated task " + id);
+                w.getTasks().get(x).dbUpdate();
+                
+                break;
+            }
+        }
+    }
+    
+    //Creates a template task using default values and returns id as String
+    public String getID()
+    {
+        long id;
+        id = ts.getID();
         
+        String s = String.valueOf(id);
+        
+        System.out.println("String id: " + s);
+        
+        return s;
+    }
+    
+    public void delete()
+    {
+        WebSockets w = Reference.w;
+       for(int x=0; x< w.getTasks().size();x++)
+       {
+            if(w.getTasks().get(x).getID().equals(deleteID))
+            {
+                long id;
+                id = Long.valueOf(deleteID).longValue();
+                
+                ts.delete(id);
+                System.out.println("Delete task " + id);
+                w.getTasks().get(x).dbUpdate();
+                
+               break;
+            }
+        }
     }
 }
